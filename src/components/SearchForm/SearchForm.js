4931a235736d38f7a81c162f3f7369727854ad29
Maqel <98158452+Maqel@users.noreply.github.com>
2022-07-27 20:30:40 +0200

    Subreducers_work

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,7 +3,7 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateSearching } from '../../redux/store';
+import { updateSearching } from '../../redux/searchStringReducer';
 const SearchForm = () => {
     const currentInputValue = useSelector(state=>state.searchString);
     const [newSearchString, setSearchString] = useState(currentInputValue);
